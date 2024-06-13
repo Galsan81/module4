0977e7afa701838d4728de7e05692b9758d1df69
Galsan81 <galsmizhit@gmail.com>
2024-06-13 10:17:08 +0800

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,11 @@
+def divide(first, second):
+    if second == 0:
+        print('Ошибка')
+        return
+    else:
+        res = first/second
+        print(res)
+        return res
+
+#divide(47, 12)
+#print(divide(45, 7))
