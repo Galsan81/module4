09abeaaaa22a082d9c8bfa01cb214de5f8546c89
Galsan81 <galsmizhit@gmail.com>
2024-06-13 10:16:33 +0800

    Add files via upload

---
+++ true_math.py
@@ -0,0 +1,14 @@
+from math import inf
+def divide(first, second):
+    if second == 0:
+        return inf
+    else:
+        re = first/second
+        print(re)
+        return re
+#(divide(32, 3))
+
+def main():
+    sd = 3
+if __name__== '__main__':
+    main()
